3f53bda6dead1fb06aa710cad52a09fd5a8227d5
Tomasz Nowak <tomasz.nowak@mycit.ie>
2022-09-30 18:09:27 +0100

    Lab 1 task 4 implemented.

---
+++ lab1/task4.py
@@ -0,0 +1,10 @@
+# task 4
+
+name = input("What is your name? \n>>>")
+surname = input("What is your surname? \n>>>")
+grade1 = int(input("Grade for the first subject? \n>>>"))
+grade2 = int(input("Grade for the first subject? \n>>>"))
+grade3 = int(input("Grade for the first subject? \n>>>"))
+AVERAGE = (grade1 + grade2 + grade3) / 3
+
+print(f"Student {name} {surname} average: {AVERAGE}")
