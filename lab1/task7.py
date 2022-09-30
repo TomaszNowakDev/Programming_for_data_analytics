d11ab81fa27aa1550a8b166bb4f24d1aa2ceb9da
Tomasz Nowak <tomasz.nowak@mycit.ie>
2022-09-30 19:22:19 +0100

    Lab 1 task 7 implemented.

---
+++ lab1/task7.py
@@ -0,0 +1,9 @@
+# task 7
+
+name = input("What is your name? \n>>>")
+age = int(input("What is your age? \n>>>"))
+months = age * 12
+days = age * 365
+
+print(name, "is", months, "months old.")
+print(name, "is", days, "days old.")
