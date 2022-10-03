183763fb6c337ce3fa7a8538f820594874175f8c
Tomasz Nowak <tomasz.nowak@mycit.ie>
2022-10-03 14:18:49 +0100

    Alias func added to the task6module for better readability.

--- lab2/task6.py
+++ lab2/task6.py
@@ -1,30 +1,30 @@
 # Lab 2 task 6
-import task6_modules
+import task6_modules as func
 
 MENU = "Would you like to perform:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n>"
 
 
 def main():
-    number1, number2 = task6_modules.get_numbers()
+    number1, number2 = func.get_numbers()
 
-    choice = task6_modules.get_choice(MENU)
+    choice = func.get_choice(MENU)
 
     while choice != 5:
         if choice == 1:
-            result = task6_modules.addition(number1, number2)
+            result = func.addition(number1, number2)
             print(f"Addition of {number1} and {number2} is {result}\n")
         elif choice == 2:
-            result = task6_modules.subtraction(number1, number2)
+            result = func.subtraction(number1, number2)
             print(f"Subtraction of {number1} and {number2} is {result}\n")
         elif choice == 3:
-            result = task6_modules.multiplication(number1, number2)
+            result = func.multiplication(number1, number2)
             print(f"Multiplication of {number1} and {number2} is {result}\n")
         elif choice == 4:
-            result = task6_modules.division(number1, number2)
+            result = func.division(number1, number2)
             print(f"Division of {number1} and {number2} is {result}\n")
 
-        number1, number2 = task6_modules.get_numbers()
-        choice = task6_modules.get_choice(MENU)
+        number1, number2 = func.get_numbers()
+        choice = func.get_choice(MENU)
 
     print("Thank you. Goodbye!")
 
