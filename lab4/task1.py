a8d34779cc1a87dd61db7487e9ee3e95c7a56b1d
Tomasz Nowak <tomasz.nowak@mycit.ie>
2022-10-13 09:03:38 +0100

    Lab 4 task 1 implemented.

---
+++ lab4/task1.py
@@ -0,0 +1,13 @@
+# Lab 4 task 1
+
+import numpy as np
+
+np_array = np.arange(81, dtype=float).reshape(9, 9)
+
+size = int(len(np_array))
+size_third = int(size/3)
+size_two_third = int(size_third * 2)
+
+print(np_array[size_third:size_two_third, size_third:size_two_third])
+
+
