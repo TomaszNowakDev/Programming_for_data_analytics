4657b2d52d8de7a571276042f20d802b6e06df28
Tomasz Nowak <tomasz.nowak@mycit.ie>
2022-10-17 20:40:05 +0100

    Lab 4 task 2 implemented.

--- lab4/task1.py
+++ lab4/task1.py
@@ -1,13 +1,12 @@
 # Lab 4 task 1
-
 import numpy as np
 
 np_array = np.arange(81, dtype=float).reshape(9, 9)
+print(np_array)
+print()
 
 size = int(len(np_array))
 size_third = int(size/3)
 size_two_third = int(size_third * 2)
 
 print(np_array[size_third:size_two_third, size_third:size_two_third])
-
-
---
+++ lab4/task2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def even_second_half(arr):
+
+    print(arr)
+    print()
+
+    second_half = int(len(arr[0])/2)
+    print(arr[2::2, second_half:])
+
+
+def main():
+    array = np.array([[14.4, 2.4, 3.5, 5.3], [54.3, 34.4, 98.22, 65.4], [100, 200, 300, 400]], float)
+    even_second_half(array)
+
+
+if __name__ == '__main__':
+    main()
